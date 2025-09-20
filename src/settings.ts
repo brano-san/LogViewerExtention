b8f3938ee46322809f8e0f12d89eb48a331619a7
brano-san <branoverv10@bk.ru>
2025-09-21 01:08:58 +0300

    Remove unused files

--- src/settings.ts
+++
@@ -1,6 +0,0 @@
-import * as vscode from 'vscode';
-
-export function getColorForLevel(level: string): string {
-  const config = vscode.workspace.getConfiguration('logviewer.color');
-  return config.get<string>(level, '#FFFFFF');
-}
